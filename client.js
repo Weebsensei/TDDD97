bc2f4a5dc1003ad20def6cb5d962ad798230a6cb
Weeb <eripe498@gitlab.liu.se>
2025-02-07 14:46:37 +0100

    Lab 1 step 3

--- client.js
+++ client.js
@@ -5,3 +5,35 @@ window.onload = function(){
 function displayView (){
     document.getElementById("viewport").innerHTML = document.getElementById("welcome_view").innerHTML;
 };
+
+function Login(form) {
+    LoginValidate(form);
+}
+
+function LoginValidate(form) {
+    let email = form.email_input.value;
+    let password = form.passwd_input.value;
+    let errorMessage = document.getElementById("login_error")
+
+    errorMessage.textContent = "app appp app. you must be getting funky ;)"
+
+    return true;
+
+}
+
+
+function signup(form){
+    signupValidation(form)
+}
+    
+function signupValidation(form) {
+    let passwd = form.passwd.value
+    let repasswd = form.repasswd.value
+
+    if (passwd !== repasswd) {
+        errorMessage.textContent = "lösenorden måste vara lika. försök igen ;)"
+        return false;
+    }
+
+    return true;
+}
