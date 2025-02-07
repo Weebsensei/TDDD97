79de0386d22eaaac3250784cea9330c411104274
Weeb <eripe498@gitlab.liu.se>
2025-02-07 13:59:23 +0100

    Lab 1 Step 2

--- client.js
+++ client.js
@@ -1,9 +1,7 @@
-// displayView = function(){
-//     // the code required to display a view
-//     };
-//     window.onload = function(){
-//     //code that is executed as the page is loaded.
-//     //You shall put your own custom code here.
-//     //window.alert() is not allowed to be used in your implementation.
-//     window.alert("Hello TDDD97!");
-//     };
+window.onload = function(){
+    displayView();
+};
+
+function displayView (){
+    document.getElementById("viewport").innerHTML = document.getElementById("welcome_view").innerHTML;
+};
